ac99970d3d51c41e0d19bed27921697ac1dd60d9
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-06 15:16:41 +0300

    added my first php file

---
+++ first.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$firstName = "Romil";
+	$lastName = "Rõbtšenkov";
+	
+	// echo trükib välja muutuja veebilehel
+	
+	//echo $firstName;
+	//echo " ";
+	//echo $lastName;
+	
+	// punkt liidab stringe
+	
+	echo $firstName." ".$lastName;
+	
+?>
